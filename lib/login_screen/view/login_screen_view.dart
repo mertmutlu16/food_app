745e26c1b557e98df2111ae97a1d2685f5e4b310
mertmutlu16 <mrt.mutlu@outlook.com>
2023-09-05 13:25:36 +0300

    login screen added

---
+++ lib/login_screen/view/login_screen_view.dart
@@ -0,0 +1,95 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class LoginScreenView extends StatefulWidget {
+  const LoginScreenView({super.key});
+
+  @override
+  State<LoginScreenView> createState() => _LoginScreenViewState();
+}
+
+class _LoginScreenViewState extends State<LoginScreenView> {
+
+  bool isObscured = false;
+  TextEditingController passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            
+            children: [
+              SizedBox(
+                height: 70,
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Text( "E-posta ve Şifreniz ile giriş yapın", 
+                  style: TextStyle(
+                    fontSize: 20 ,
+                    fontWeight: FontWeight.bold,),
+                ),
+              ),
+              
+              SizedBox(
+                height: 20,
+              ),
+
+              TextField(              
+                decoration: InputDecoration(
+                  labelText: "E-posta" ,
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black54),
+                  ),
+                  
+                  ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(              
+                controller: passwordController,
+                obscureText: isObscured ? false : true,
+                decoration: InputDecoration(
+                  labelText: "Şifre" ,
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black54),
+                  ),
+                  suffixIcon:GestureDetector(
+                    onTap: (){
+                      setState(() {
+                        isObscured = !isObscured;
+                        
+                      });
+                    },
+                    child : isObscured ? Icon(Icons.visibility) : Icon(Icons.visibility_off),
+                  ) 
+                  ),
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.only(top: 8),
+                child: Text("Şifremi unuttum" , style: const TextStyle(color: Color.fromARGB(1000,241,0,77) ,fontSize: 16),)),
+              Spacer(),
+              ElevatedButton(onPressed: (){},
+              child: const Text("Devam et"),
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(double.infinity, 50),
+                backgroundColor:  Color.fromARGB(1000,241,0,77),
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              )
+              
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/login_screen/view_model/login_screen_view_model.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/login_screen/view/login_screen_view.dart';
+
+class LoginScreenViewModel{
+
+
+  Widget buildLoginScreen(BuildContext context){
+    return const LoginScreenView();
+  } 
+}
