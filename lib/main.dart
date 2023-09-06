22eacebf40e0f8fea45c0f6b7f509608388d7fdc
mertmutlu16 <mrt.mutlu@outlook.com>
2023-09-06 10:41:56 +0300

    updated main

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/login_screen/view_model/login_screen_view_model.dart';
 import 'package:food_app/splash_screen/view_model/splash_view_model.dart';
 
 void main() {
-  runApp( const MyApp());
+  runApp( MyApp());
 }
 
-class MyApp extends StatefulWidget {
-   const MyApp({super.key});
+class MyApp extends StatelessWidget {
+   MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
- 
-
-class _MyAppState extends State<MyApp> {
-  final splashViewModel = SplashScreenViewModel();
-
-  @override
-  void initState(){
-    super.initState();
-
-    Future.delayed(const Duration(seconds: 3)).then((value) {
-      // Add login page
-      //Navigator.pushReplacement(context, MaterialPageRoute(builder: (contex)=>LoginPage));
-    });
-
-  }
-
-
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Food App',
+      title: 'Food APP',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: splashViewModel.buildSplashScreen(context), 
+      home: MyHomePage(),
     );
   }
 }
 
 
+class MyHomePage extends StatefulWidget {
+  
 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+    SplashScreenViewModel splashViewModel = SplashScreenViewModel();
+    LoginScreenViewModel loginScreenViewModel = LoginScreenViewModel();
+
+  @override
+  void initState() {
+    super.initState();
+    
+    Future.delayed(const Duration(seconds: 3)).then((value){
+
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context)=> loginScreenViewModel.buildLoginScreen(context)));
+    });
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: splashViewModel.buildSplashScreen(context),
+      );
+  }
+}
