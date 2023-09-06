7a34e10df8bd49f6aa4ef083b5ac05a100aa2ce0
mertmutlu16 <mrt.mutlu@outlook.com>
2023-09-06 10:42:44 +0300

    added main screen

---
+++ lib/main_screen/view_model/main_screen_view_model.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/main_screen/view/main_screen_view.dart';
+
+class MainScreenViewModel {
+
+
+  Widget buildingMainScreen(BuildContext context){
+    return MainScreenView();
+  }
+}
