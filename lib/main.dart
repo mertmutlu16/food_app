6dea714d8f2e3fdb4755959e7275e52cbbc59fe1
mertmutlu16 <mrt.mutlu@outlook.com>
2023-09-07 15:44:05 +0300

    Added GetMaterialApp for route management

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,63 @@
 import 'package:flutter/material.dart';
-import 'package:food_app/login_screen/view_model/login_screen_view_model.dart';
-import 'package:food_app/splash_screen/view_model/splash_view_model.dart';
+import 'package:food_app/detail_screen/view/detail_screen_view.dart';
+import 'package:food_app/main_screen/view/main_screen_view.dart';
+import 'package:food_app/splash_screen/view/splash_screen_view.dart';
+import 'package:flutter/services.dart';
+import 'package:food_app/utils/extentions/scroll_behavior.dart';
+import 'package:food_app/utils/locator.dart';
+import 'package:food_app/utils/navigation_service.dart';
+import 'package:food_app/utils/routes/app_router.dart';
+import 'package:food_app/utils/routes/app_routes.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:sizer/sizer.dart';
 
 void main() {
+  setupLocater();
   runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
    MyApp({super.key});
 
+  NavigationService navigationService = NavigationService();
+
   
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Food APP',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
+
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return GetMaterialApp(
+          scrollBehavior: CustomScrollBehavior(),
+          theme: ThemeData(
+            
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          getPages: AppRouter.getPages,
+          initialRoute: AppRouter.initialPath,
+          unknownRoute: AppRoutes.unknownRoute,
+        );
         
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: MyHomePage(),
+        
+      /*  MaterialApp(
+           
+          title: 'Food APP',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home:const  SplashScreenView(),
+          
+        );*/
+      }
     );
-  }
-}
-
-
-class MyHomePage extends StatefulWidget {
-  
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  
-    SplashScreenViewModel splashViewModel = SplashScreenViewModel();
-    LoginScreenViewModel loginScreenViewModel = LoginScreenViewModel();
-
-  @override
-  void initState() {
-    super.initState();
-    
-    Future.delayed(const Duration(seconds: 3)).then((value){
-
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context)=> loginScreenViewModel.buildLoginScreen(context)));
-    });
-
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return  Scaffold(
-      body: splashViewModel.buildSplashScreen(context),
-      );
   }
 }
