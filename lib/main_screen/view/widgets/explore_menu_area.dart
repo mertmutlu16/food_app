91d1cfd83968469ac45ff8818eab6c0c019a4ce0
mertmutlu16 <mrt.mutlu@outlook.com>
2023-09-18 11:30:33 +0300

    adding mobx state managmnet

--- lib/main_screen/view/widgets/explore_menu_area.dart
+++ lib/main_screen/view/widgets/explore_menu_area.dart
@@ -1,10 +1,9 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:food_app/main_screen/view_model/main_screen_view_model.dart';
 import 'package:food_app/utils/routes/app_routes.dart';
 import 'package:get/get.dart';
 import 'package:sizer/sizer.dart';
-import 'package:http/http.dart' as http;
 import '../../../utils/models/foods.dart';
 
 class ExploreMenuArea extends StatefulWidget {
@@ -19,17 +18,12 @@ class ExploreMenuArea extends StatefulWidget {
 class _ExploreMenuAreaState extends State<ExploreMenuArea> {
   List<Foods> foodsList = [];
 
-  //static const _apiRoute="https://localhost:44333/api/foods";
-
+  /*
   Future<List<Foods>> getAllFoods() async {
 
-    
-    //var url = "https://10.0.2.2:8000/api/foods";
     var url = "http://192.168.169.154:44333/api/foods";
-    //var url = "https://192.168.169.154:44333/api/foods";
     var response = await http.get(Uri.parse(url));
     
-    //.catchError((err){debugPrint(err.toString());});
     
 
 
@@ -47,25 +41,15 @@ class _ExploreMenuAreaState extends State<ExploreMenuArea> {
     } else {
       throw Exception('Veri çekme başarısız: ${response.statusCode}');
     }
-  }
-  
-  @override
-  void initState(){
-    super.initState();
-    getAllFoods();
-  }
+  }*/
 
-/*
-  var foods = [
-    "Kuzu Şiş Kebap",
-    "Deniz Mahsulleri Kavurma",
-    "Tost",
-    "Sushi",
-    "Baklava",
-    "Tavuk Izgara",
-    "Hamburger",
-    "Makarna"
-  ];*/
+  MainScreenViewModel mainScreenViewModel = MainScreenViewModel();
+
+  @override
+  void initState() {
+    super.initState();
+    mainScreenViewModel.getAllFoods();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -91,42 +75,59 @@ class _ExploreMenuAreaState extends State<ExploreMenuArea> {
             ),
             Expanded(
               flex: 4,
-              child: ListView.builder(
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  itemCount: foodsList.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: EdgeInsets.all(.1.h),
-                      child: SizedBox(
-                        width: 35.w,
-                        child: GestureDetector(
-                          onTap: () {
-                            Get.toNamed(AppRoutes.DETAIL_SCREEN_PATH);
-                          },
-                          child: Card(
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20)),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Expanded(
-                                    child: ClipRRect(
-                                        borderRadius: BorderRadius.circular(20),
-                                        child: Image.asset(
-                                            "images/hamburger.jpeg"))),
-                                Text(
-                                  foodsList[index].foodName,
-                                  style: const TextStyle(
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
+              child: Observer(
+                builder: (_) {
+                  if (foodsList.isEmpty) {
+                    return SizedBox(
+                      height: 10.h,
+                      width: 36.w,
+                      child: const CircularProgressIndicator(
+                         valueColor: AlwaysStoppedAnimation<Color>(Color.fromARGB(1000, 241, 0, 77)),
                       ),
                     );
-                  }),
+                  } else {
+                    return ListView.builder(
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        itemCount: mainScreenViewModel.foodsList.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: EdgeInsets.all(.1.h),
+                            child: SizedBox(
+                              width: 35.w,
+                              child: GestureDetector(
+                                onTap: () {
+                                  Get.toNamed(AppRoutes.DETAIL_SCREEN_PATH);
+                                },
+                                child: Card(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20)),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      Expanded(
+                                          child: ClipRRect(
+                                              borderRadius:
+                                                  BorderRadius.circular(20),
+                                              child: Image.asset(
+                                                  "images/hamburger.jpeg"))),
+                                      Text(
+                                        mainScreenViewModel
+                                            .foodsList[index].foodName,
+                                        style: const TextStyle(
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          );
+                        });
+                  }
+                },
+              ),
             ),
           ],
         ),
--- lib/main_screen/view_model/main_screen_view_model.dart
+++ lib/main_screen/view_model/main_screen_view_model.dart
@@ -1,10 +1,41 @@
 // ignore_for_file: library_private_types_in_public_api
 
+import 'dart:convert';
+
 import 'package:mobx/mobx.dart';
+import 'package:http/http.dart' as http;
+import '../../utils/models/foods.dart';
 part 'main_screen_view_model.g.dart';
 
 class MainScreenViewModel = _MainScreenViewModelBase with _$MainScreenViewModel;
 
 abstract class _MainScreenViewModelBase with Store {
   
+  @observable
+  ObservableList<Foods> foodsList = ObservableList<Foods>();
+
+
+
+
+   @action
+    Future<List<Foods>> getAllFoods() async {
+
+    var url = "http://192.168.169.154:44333/api/foods";
+    var response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      var data = json.decode(response.body).toList();
+
+      for (var item in data) {
+        var food = Foods.fromJson(item);
+        foodsList.add(food);
+      } 
+      return foodsList;
+    } else {
+      throw Exception('Veri çekme başarısız: ${response.statusCode}');
+    }
+    
+    
+  }
+
 }
