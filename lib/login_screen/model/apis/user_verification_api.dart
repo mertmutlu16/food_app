8025a368528e9dd8147c58b350885542c704bc66
mertmutlu16 <mrt.mutlu@outlook.com>
2023-10-15 18:30:54 +0300

    tracked images

--- lib/login_screen/model/apis/user_verification_api.dart
+++ lib/login_screen/model/apis/user_verification_api.dart
@@ -7,12 +7,14 @@ class UserVerificationApi{
 
   Future userVeriff(String email , String password) async{
    
-    var url = "http://192.168.169.154:44333/api/users/userVerification";
+    var url = "https://192.168.169.154:44333/api/users/userVerification";
     var userModel = {"Fullname":"","Email":email,"Password":password};
     var userModelJson = jsonEncode(userModel);
+    print("RESPONSE ÖNCESİ");
     var response = await http.post(Uri.parse(url) , body:userModelJson,headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },);
+    print("RESPONSE :  $response");
 
     if(response.statusCode == 200){
       var jsonData = json.decode(response.body) ;
